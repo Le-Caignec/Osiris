bbefe2a1b697e8cb6ad29845faf51714dd83b201
Le-Caignec <robinlecaignec@yahoo.fr>
2025-09-25 22:48:28 +0200

    Add Osiris management commands and update callback contract address
    - Introduced a new Makefile command to trigger the Osiris callback on Sepolia/Ethereum.
    - Added new JSON files for transaction receipts related to the Osiris callback.
    - Updated the callback contract address in the configuration file.
    - Created a new script for executing the Osiris callback function.

---
+++ packages/smart-contract/script/OsirisAction.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.22;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Osiris} from "../src/Osiris.sol";
+import {ConfigLib} from "./lib/configLib.sol";
+
+contract TriggerOsirisCallback is Script {
+    function run() external {
+ string memory chain = vm.envString("CHAIN");
+        ConfigLib.DestinationNetworkConfig memory config = ConfigLib.readDestinationNetworkConfig(chain);
+        address osirisContractAddress = config.callbackContract;
+
+        vm.startBroadcast();
+        // Deploy the Callback contract on Arbitrum Sepolia
+        Osiris osiris = Osiris(payable(osirisContractAddress));
+        osiris.callback();
+        console.log("Paused CronReactive at address:", osirisContractAddress);
+        vm.stopBroadcast();
+    }
+}
