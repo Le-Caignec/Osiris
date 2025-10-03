c62f294579e2fb5c868d60eecad9361b0938e2b2
Le-Caignec <robinlecaignec@yahoo.fr>
2025-10-03 20:17:54 +0200

    fix: Correct variable name from conTopic to cronTopic in CronReactive contract

--- packages/smart-contract/src/CronReactive.sol
+++ packages/smart-contract/src/CronReactive.sol
@@ -5,14 +5,14 @@ import {ISystemContract} from "@reactive-contract/interfaces/ISystemContract.sol
 import {AbstractPausableReactive} from "@reactive-contract/abstract-base/AbstractPausableReactive.sol";
 
 contract CronReactive is AbstractPausableReactive {
-    uint256 public conTopic;
+    uint256 public cronTopic;
     uint64 private constant GAS_LIMIT = 1000000;
     uint256 public destinationChainId;
     address private callback;
 
     constructor(address _service, uint256 _cronTopic, uint256 _destinationChainId, address _callback) payable {
         service = ISystemContract(payable(_service));
-        conTopic = _cronTopic;
+        cronTopic = _cronTopic;
         destinationChainId = _destinationChainId;
         callback = _callback;
 
@@ -26,12 +26,12 @@ contract CronReactive is AbstractPausableReactive {
     function getPausableSubscriptions() internal view override returns (Subscription[] memory) {
         Subscription[] memory result = new Subscription[](1);
         result[0] =
-            Subscription(block.chainid, address(service), conTopic, REACTIVE_IGNORE, REACTIVE_IGNORE, REACTIVE_IGNORE);
+            Subscription(block.chainid, address(service), cronTopic, REACTIVE_IGNORE, REACTIVE_IGNORE, REACTIVE_IGNORE);
         return result;
     }
 
     function react(LogRecord calldata log) external vmOnly {
-        if (log.topic_0 == conTopic) {
+        if (log.topic_0 == cronTopic) {
             bytes memory payload = abi.encodeWithSignature("callback(address)", address(0));
             emit Callback(destinationChainId, callback, GAS_LIMIT, payload);
         }
