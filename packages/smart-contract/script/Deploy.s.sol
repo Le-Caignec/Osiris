5e1564b4fb530c1a35141b251a09f11ba019ddd9
Le-Caignec <robinlecaignec@yahoo.fr>
2025-10-23 18:14:19 +0200

    feat: enhance deployment configurations for Ledger integration
    - Updated .env.template to include a new LEDGER_INDEX variable for specifying the Ledger account index.
    - Modified Makefile to utilize the LEDGER_INDEX for deploying contracts, improving flexibility in Ledger account selection.
    - Cleaned up deployment scripts by removing unnecessary conditions and ensuring consistent use of the Ledger address and index.

--- packages/smart-contract/script/Deploy.s.sol
+++ packages/smart-contract/script/Deploy.s.sol
@@ -8,19 +8,19 @@ import {Osiris} from "../src/Osiris.sol";
 import {ConfigLib} from "./lib/configLib.sol";
 
 // TODO: To remove
-contract DeployCallback is Script {
-    function run() external {
-        string memory chain = vm.envString("CHAIN");
-        ConfigLib.DestinationNetworkConfig memory config = ConfigLib.readDestinationNetworkConfig(chain);
-        address callbackSender = config.callbackProxyContract;
+// contract DeployCallback is Script {
+//     function run() external {
+//         string memory chain = vm.envString("CHAIN");
+//         ConfigLib.DestinationNetworkConfig memory config = ConfigLib.readDestinationNetworkConfig(chain);
+//         address callbackSender = config.callbackProxyContract;
 
-        vm.startBroadcast();
-        // Deploy the Callback contract on Sepolia
-        Callback callbackContract = new Callback{value: 0.000001 ether}(callbackSender);
-        console.log("Callback Contract Address (Sepolia):", address(callbackContract));
-        vm.stopBroadcast();
-    }
-}
+//         vm.startBroadcast();
+//         // Deploy the Callback contract on Sepolia
+//         Callback callbackContract = new Callback{value: 0.000001 ether}(callbackSender);
+//         console.log("Callback Contract Address (Sepolia):", address(callbackContract));
+//         vm.stopBroadcast();
+//     }
+// }
 
 contract DeployOsiris is Script {
     function run() external {
@@ -35,8 +35,7 @@ contract DeployOsiris is Script {
 
         vm.startBroadcast();
         // Deploy the Osiris contract with Chainlink integration
-        Osiris osirisContract =
-            new Osiris{value: 0.01 ether}(universalRouter, permit2, usdc, callbackSender, ethUsdFeed);
+        Osiris osirisContract = new Osiris(universalRouter, permit2, usdc, callbackSender, ethUsdFeed);
         console.log("Osiris Contract Address:", address(osirisContract));
         vm.stopBroadcast();
     }
