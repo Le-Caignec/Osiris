73e944834589cc2b787aa98d3953cae48d4d0e98
Le-Caignec <robinlecaignec@yahoo.fr>
2025-10-26 14:55:03 +0100

    feat: enhance Osiris deployment with callback proxy funding
    - Added functionality to prompt for funding the callback proxy during the Osiris contract deployment.
    - Implemented logic in the deployment script to deposit ETH to the callback proxy if a funding amount is specified.
    - Updated Makefile to pass the funding amount to the deployment script, improving user interaction and contract setup.

--- packages/smart-contract/script/Deploy.s.sol
+++ packages/smart-contract/script/Deploy.s.sol
@@ -6,6 +6,10 @@ import {CronReactive} from "../src/CronReactive.sol";
 import {Osiris} from "../src/Osiris.sol";
 import {ConfigLib} from "./lib/configLib.sol";
 
+interface ICallbackProxy {
+    function depositTo(address payee) external payable;
+}
+
 contract DeployOsiris is Script {
     function run() external {
         string memory chain = vm.envString("CHAIN");
@@ -18,9 +22,21 @@ contract DeployOsiris is Script {
         address ethUsdFeed = config.chainlinkEthUsdFeed;
 
         vm.startBroadcast();
+
         // Deploy the Osiris contract with Chainlink integration
         Osiris osirisContract = new Osiris(universalRouter, permit2, usdc, callbackSender, ethUsdFeed);
         console.log("Osiris Contract Address:", address(osirisContract));
+
+        // Fund the callback proxy if FUND_AMOUNT is set
+        uint256 fundAmount = vm.envOr("FUND_AMOUNT", uint256(0));
+        if (fundAmount > 0) {
+            console.log("Funding callback proxy with", fundAmount, "wei");
+            ICallbackProxy(callbackSender).depositTo{value: fundAmount}(address(osirisContract));
+            console.log("Successfully funded callback proxy");
+        } else {
+            console.log("No funding requested (FUND_AMOUNT not set or is 0)");
+        }
+
         vm.stopBroadcast();
     }
 }
